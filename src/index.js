0fbe81f033cc21b50bae172e2ee868971e2bf9f9
Ivan Lisin <ivanlisin.tech@gmail.com>
2020-02-11 01:30:03 +0700

    update index.js & delete engine.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import readlineSync from 'readline-sync';
-import start from './engine';
+import { car as getDescription, cdr as getResult } from '@hexlet/pairs';
+
+const attemptsCount = 3;
 
 export default (intro, getTask) => {
   console.log(`Welcome to the Brain Games!\n${intro}\n`);
@@ -7,7 +9,23 @@ export default (intro, getTask) => {
   const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}!\n`);
 
-  start(username, getTask);
+  for (let count = 0; count < attemptsCount;) {
+    const task = getTask();
+
+    const answer = readlineSync.question(`Question: ${getDescription(task)}\nYour answer: `);
+    const result = getResult(task);
+
+    switch (answer === result) {
+      case true:
+        console.log('Correct!');
+        count += 1;
+        break;
+      default:
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'\nLet's try again, ${username}!`);
+        count = 0;
+        break;
+    }
+  }
 
   console.log(`Congratulations, ${username}!`);
 };
